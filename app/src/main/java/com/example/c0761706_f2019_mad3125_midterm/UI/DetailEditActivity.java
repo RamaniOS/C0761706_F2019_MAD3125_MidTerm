84e69b630c55642fb21ba326e6749bb6dc137c33
Ramanpreet Singh <ramanpreet230@gmail.com>
2019-12-01 11:19:23 -0500

    Add DatePicker dialog.

--- app/src/main/java/com/example/c0761706_f2019_mad3125_midterm/UI/DetailEditActivity.java
+++ app/src/main/java/com/example/c0761706_f2019_mad3125_midterm/UI/DetailEditActivity.java
@@ -2,12 +2,17 @@ package com.example.c0761706_f2019_mad3125_midterm.UI;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 
 import com.example.c0761706_f2019_mad3125_midterm.R;
 import com.google.android.material.textfield.TextInputEditText;
+
+import java.util.Calendar;
 
 public class DetailEditActivity extends AppCompatActivity {
 
@@ -18,6 +23,7 @@ public class DetailEditActivity extends AppCompatActivity {
     private TextInputEditText txtGrossIncome;
     private TextInputEditText txtRRSP;
     private Button btnCalculate;
+    private DatePickerDialog datePickerDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,12 +40,35 @@ public class DetailEditActivity extends AppCompatActivity {
         txtGrossIncome = findViewById(R.id.txtGross);
         txtRRSP = findViewById(R.id.txtRRSP);
         btnCalculate = findViewById(R.id.btnCalculate);
+        txtBirthDate.setInputType(InputType.TYPE_NULL);
+        txtBirthDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openDatePicker();
+            }
+        });
         btnCalculate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 calculateButtonClicked();
             }
         });
+    }
+
+    private void openDatePicker() {
+        final Calendar cldr = Calendar.getInstance();
+        int day = cldr.get(Calendar.DAY_OF_MONTH);
+        int month = cldr.get(Calendar.MONTH);
+        int year = cldr.get(Calendar.YEAR);
+        // date picker dialog
+        datePickerDialog = new DatePickerDialog(DetailEditActivity.this,
+                new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                        txtBirthDate.setText(dayOfMonth + "/" + (monthOfYear + 1) + "/" + year);
+                    }
+                }, year, month, day);
+        datePickerDialog.show();
     }
 
     private void calculateButtonClicked() {
