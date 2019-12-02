fd8b86b726622b10c59d3660f8f9169d8a78e2d4
Ramanpreet Singh <ramanpreet230@gmail.com>
2019-12-02 12:53:25 -0500

    Added menu to show add option to open edit screen.

--- app/src/main/java/com/example/c0761706_f2019_mad3125_midterm/UI/HomeActivity.java
+++ app/src/main/java/com/example/c0761706_f2019_mad3125_midterm/UI/HomeActivity.java
@@ -1,10 +1,15 @@
 package com.example.c0761706_f2019_mad3125_midterm.UI;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.example.c0761706_f2019_mad3125_midterm.Adapter.HomeAdapter;
 import com.example.c0761706_f2019_mad3125_midterm.Models.HomeDetail;
@@ -36,5 +41,23 @@ public class HomeActivity extends AppCompatActivity {
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
         homeRecyclerView.setLayoutManager(layoutManager);
         homeRecyclerView.setAdapter(homeAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_add, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_add:
+                Intent intent = new Intent(this, DetailEditActivity.class);
+                startActivity(intent);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
