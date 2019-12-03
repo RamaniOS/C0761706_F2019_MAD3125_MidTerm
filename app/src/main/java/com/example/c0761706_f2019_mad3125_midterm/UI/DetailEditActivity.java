3a44689fc9921bae822722fb6d645af494fcb8d6
Ramanpreet Singh <ramanpreet230@gmail.com>
2019-12-02 19:12:04 -0500

    Changes in Detail edit Activity.

--- app/src/main/java/com/example/c0761706_f2019_mad3125_midterm/UI/DetailEditActivity.java
+++ app/src/main/java/com/example/c0761706_f2019_mad3125_midterm/UI/DetailEditActivity.java
@@ -7,7 +7,6 @@ import android.app.DatePickerDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Parcelable;
 import android.text.InputType;
